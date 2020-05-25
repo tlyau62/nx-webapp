f9051414723c53f8b26f5b0c1462ae1bcc9fb325
Tim Yau <tyautl@nexify.com.hk>
2020-05-25 11:35:21 +0800

    Add icons panel

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,61 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div id="icons-panel">
+      <i class="fas fa-home"></i>
+      <i class="fas fa-box"></i>
+
+      <div class="bottom-icons">
+        <i class="fas fa-trash-alt"></i>
+        <i class="fas fa-cogs"></i>
+      </div>
     </div>
-    <router-view/>
+    <div id="main-panel">
+      <p>test</p>
+    </div>
   </div>
 </template>
 
 <style lang="scss">
+@import "node_modules/bootstrap/scss/bootstrap";
+
+html,
+body {
+  height: 100%;
+  width: 100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  height: 100%;
+  display: flex;
+  flex-direction: row;
 }
 
-#nav {
-  padding: 30px;
+#icons-panel {
+  display: flex;
+  flex-direction: column;
+  width: 48px;
+  background-color: theme-color("dark");
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+  .bottom-icons {
+    @extend #icons-panel;
+    position: absolute;
+    bottom: 0;
+  }
 
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  i {
+    display: flex;
+    font-size: 20px;
+    width: 48px;
+    height: 48px;
+    align-items: center;
+    justify-content: center;
+    color: theme-color("light");
+    opacity: 60%;
   }
 }
 </style>
+
+<style src="@fortawesome/fontawesome-free/css/all.css"></style>
