ef92aaf96814ff56e745938d161cca49ff389569
Tim Yau <tyautl@nexify.com.hk>
2020-06-04 15:26:16 +0800

    Add label

--- src/components/table-ctx-menu/TableCtxMenu.vue
+++ src/components/table-ctx-menu/TableCtxMenu.vue
@@ -9,6 +9,15 @@
     <ctx-menu-item>
       <i class="fas fa-edit"></i>Edit
     </ctx-menu-item>
+    <ctx-menu-item>
+      <i class="fas fa-file-download"></i>
+      Label
+      <ctx-menu>
+        <div class="labelContainer">
+          <div class="label" v-for="label in labels" :key="label" :style="{background: label}"></div>
+        </div>
+      </ctx-menu>
+    </ctx-menu-item>
     <ctx-menu-separator />
     <ctx-menu-item>
       <i class="fas fa-file-download"></i>
@@ -45,6 +54,21 @@ export default {
   name: "TableCtxMenu",
   components: {
     ...CtxMenu
+  },
+  data() {
+    return {
+      labels: new Array(20).fill(0).map(e => this.getRandomColor())
+    };
+  },
+  methods: {
+    getRandomColor() {
+      var letters = "0123456789ABCDEF";
+      var color = "#";
+      for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+      }
+      return color;
+    }
   }
 };
 </script>
@@ -56,6 +80,24 @@ export default {
   i {
     padding-right: 10px;
     color: $gray-500;
+  }
+
+  .labelContainer {
+    display: flex;
+    justify-content: space-between;
+    flex-wrap: wrap;
+    padding: 0 5px;
+
+    .label {
+      display: inline-block;
+      width: 25px;
+      height: 25px;
+      margin: 5px;
+
+      &:hover {
+        @extend .shadow;
+      }
+    }
   }
 }
 
