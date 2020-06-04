24bb926d0c49a9e34b9e7007803f3a06b0bbe08f
Tim Yau <tyautl@nexify.com.hk>
2020-06-04 15:01:52 +0800

    Add ctx menu to js tree

--- src/components/js-tree/JsTree.vue
+++ src/components/js-tree/JsTree.vue
@@ -7,11 +7,33 @@
 <script>
 import $ from "jquery";
 import "jstree/dist/jstree";
+import JstreeCtxMenu from "@/components/table-ctx-menu/JstreeCtxMenu";
+import CtxMenuEvtBus from "nex-ctxmenu/src/components/ctx-menu/ctx-menu-evt-bus";
 
 export default {
   props: ["options"],
   mounted() {
     $(this.$el).jstree(this.options || {});
+
+    $(this.$el).bind("contextmenu.jstree", event => {
+      event.preventDefault();
+      event.stopPropagation();
+      var node = $(event.target).closest("li");
+      var data = node.data("jstree");
+      const treeDim = window.document
+        .querySelector("html")
+        .getBoundingClientRect();
+
+      CtxMenuEvtBus.$emit("open", {
+        menuComp: JstreeCtxMenu,
+        pos: {
+          x: event.originalEvent.x - treeDim.left,
+          y: event.originalEvent.y - treeDim.top
+        },
+        container: window.document.querySelector("html")
+      });
+      // Do some action
+    });
   }
 };
 </script>
---
+++ src/components/table-ctx-menu/JstreeCtxMenu.vue
@@ -0,0 +1,68 @@
+<template>
+  <ctx-menu class="icon-ctx-menu">
+    <ctx-menu-item>
+      <i class="far fa-folder-open"></i>Open
+    </ctx-menu-item>
+    <ctx-menu-separator />
+    <ctx-menu-item>
+      <i class="fas fa-info-circle"></i>Info
+    </ctx-menu-item>
+    <ctx-menu-item>
+      <i class="far fa-trash-alt"></i>Delete
+    </ctx-menu-item>
+    <ctx-menu-item>
+      <i class="fas fa-edit"></i>Edit
+    </ctx-menu-item>
+    <ctx-menu-separator />
+    <ctx-menu-item>
+      <i class="fas fa-file-download"></i>
+      Download
+      <ctx-menu>
+        <ctx-menu-item>As zip</ctx-menu-item>
+        <ctx-menu-item>
+          As 7z
+          <ctx-menu>
+            <ctx-menu-item>High compress ratio</ctx-menu-item>
+            <ctx-menu-item>Normal compress ratio</ctx-menu-item>
+            <ctx-menu-item>Low compress ratio</ctx-menu-item>
+          </ctx-menu>
+        </ctx-menu-item>
+        <ctx-menu-item>Normally</ctx-menu-item>
+      </ctx-menu>
+    </ctx-menu-item>
+    <ctx-menu-item>
+      <i class="fas fa-location-arrow"></i>
+      Workflow
+      <ctx-menu>
+        <ctx-menu-item>Workflow item 1</ctx-menu-item>
+        <ctx-menu-item>Workflow item 2</ctx-menu-item>
+        <ctx-menu-item>Workflow item 3</ctx-menu-item>
+      </ctx-menu>
+    </ctx-menu-item>
+  </ctx-menu>
+</template>
+
+<script>
+import CtxMenu from "nex-ctxmenu/src/components/ctx-menu";
+
+export default {
+  name: "TableCtxMenu",
+  components: {
+    ...CtxMenu
+  }
+};
+</script>
+
+<style scoped lang="scss">
+@import "~bootstrap";
+
+.icon-ctx-menu {
+  i {
+    padding-right: 10px;
+    color: $gray-500;
+  }
+}
+
+.ctx-menu {
+}
+</style>
